dca5758ac342c055b2ba1eb6405b606da0031c0a
John O'Sullivan <john@eximchain.com>
2019-10-18 18:32:43 -0400

    Created a wrapper method for track which is guaranteed not to throw

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -2,6 +2,7 @@ import Analytics from 'analytics-node';
 import DappbotAPI from '@eximchain/dappbot-api-client';
 import { npmPackage } from "../cli";
 import { CreateDapp, UpdateDapp } from '@eximchain/dappbot-types/spec/methods/private';
+import { cleanExit } from './util';
 
 const WRITE_KEY = 'aMv7BUcQfCSy8XYMBgXYMdLMr8fkhI4a';
 const analytics = new Analytics(WRITE_KEY, { flushAt: 1})
@@ -16,29 +17,63 @@ function standardTrackProps(API:DappbotAPI, extraProps?:SharedTrackProps) {
 }
 
 interface SharedTrackProps {
-  isTruffle?: boolean
+  isTruffle?: boolean,
+  exitOnFlush?: boolean
+}
+
+interface TrackOptions {
+  exitOnFlush?: boolean
+}
+
+// safeTrack
+// 1. Verify that the final userId is non-empty, bail &otherwise
+// 2. Perform the actual call inside of a try-catch
+// 3. Log on failure to send, but that's it.
+// 4. If we have exit on flush, do that.
+const safeTrack:Analytics['track'] = (message, cb, opts?: TrackOptions) => {
+
+  // Silently bail out if there's no userId, like on signup.
+  if (!message.userId || message.userId === '') return analytics;
+
+  try {
+    return analytics.track(message,(err, data) => {
+      if (opts && opts.exitOnFlush) cleanExit('')
+      if (cb) cb(err, data);
+    });
+  } catch (err) {
+    console.log(`Failed to send the below analytics to Segment:`)
+    console.log(message);
+    console.log(`Error Message: ${err.message}`)
+    return analytics;
+  }
 }
 
 export function trackSignup(API:DappbotAPI, email:string, metadata:Record<string, any>, extraProps?:SharedTrackProps) {
-  analytics.identify({
-    userId: email,
-    traits: metadata
-  })
-  analytics.track({
+  try {
+    analytics.identify({
+      userId: email,
+      traits: metadata
+    })
+  } catch (err) {
+    console.log('Err on analytics.identify(): ',err);
+  }
+  safeTrack({
     event: 'User Signup',
     userId: email,
     properties: {
       ...standardTrackProps(API, extraProps),
-      // Manually setting email because we do not
-      // expect the API to have any valid auth
-      // within this flow.
-      email, name
+      ...metadata,
+      // Manually setting email at the end because 
+      // we do not expect the API to have any valid 
+      // auth within this flow, so the email from
+      // standardTrackProps would be incorrect.
+      email
     }
   })
 }
 
 export function trackLogin(API:DappbotAPI, isRefresh:boolean, extraProps?:SharedTrackProps) {
-  analytics.track({
+  safeTrack({
     userId: API.authData.User.Email,
     event: 'User Login',
     properties: {
@@ -50,7 +85,7 @@ export function trackLogin(API:DappbotAPI, isRefresh:boolean, extraProps?:Shared
 
 type TrackedCreateProps = Omit<CreateDapp.Args, 'Abi'|'GuardianURL'>
 export function trackCreateDapp(API:DappbotAPI, DappName:string, createArgs:TrackedCreateProps, extraProps?:SharedTrackProps) {
-  analytics.track({
+  safeTrack({
     event: 'Dapp Created',
     userId: API.authData.User.Email,
     properties: {
@@ -61,7 +96,7 @@ export function trackCreateDapp(API:DappbotAPI, DappName:string, createArgs:Trac
 }
 
 export function trackDeleteDapp(API:DappbotAPI, DappName:string, extraProps?:SharedTrackProps) {
-  analytics.track({
+  safeTrack({
     event: 'Dapp Deleted',
     userId: API.authData.User.Email,
     properties: {
@@ -73,7 +108,7 @@ export function trackDeleteDapp(API:DappbotAPI, DappName:string, extraProps?:Sha
 
 type TrackedUpdateProps = Omit<UpdateDapp.Args, 'Abi'>
 export function trackUpdateDapp(API:DappbotAPI, DappName:string, updateArg:TrackedUpdateProps, extraProps?:SharedTrackProps) {
-  analytics.track({
+  safeTrack({
     event: 'Dapp Updated',
     userId: API.authData.User.Email,
     properties: {
